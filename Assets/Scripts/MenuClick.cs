c426c2bb07ab07d3ca2c907daa4c801a69e45a57
ToddMatteson <toddmatteson@yahoo.com>
2019-04-26 18:03:12 -0600

    Main Menu and Options now slide onto level from above

--- Assets/Scripts/MenuClick.cs
+++ Assets/Scripts/MenuClick.cs
@@ -2,16 +2,87 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MenuClick : MonoBehaviour
+namespace PipeCircles
 {
-	[SerializeField] GameObject mainMenuPanel = null;
-
-	public void OnMouseUp()
+	public class MenuClick : MonoBehaviour
 	{
-		print("reached OnMouseUp");
-		if (mainMenuPanel != null)
+		[SerializeField] GameObject menuCanvas = null;
+		[SerializeField] [Range(0.1f, 5f)] float totalMovementTime = 1f;
+
+		Transform canvasTransform;
+		float currentMovementTime;
+		bool moveCanvas = false;
+		bool isCanvasUp = true;
+		bool canMove = false;
+		float yMoveAmount = 1080f;
+		Vector3 upPos;
+		Vector3 downPos;
+
+		private void Awake()
 		{
-			mainMenuPanel.SetActive(true);
+			if (menuCanvas == null)
+			{
+				Debug.Log("Please add a canvas to the button");
+			}
+			{
+				canvasTransform = menuCanvas.transform;
+				upPos = canvasTransform.position;
+				downPos = upPos + yMoveAmount * Vector3.down;
+			}
+		}
+
+		public void Update()
+		{
+			if (canMove)
+			{
+				MoveCanvas(!isCanvasUp);
+			}
+		}
+
+		public void OnMouseUp()
+		{
+			if (canvasTransform != null && !moveCanvas)
+			{
+				moveCanvas = true;
+				canMove = true;
+				if (isCanvasUp)
+				{
+					canvasTransform.gameObject.SetActive(true);	
+					MoveCanvas(false);
+				} else
+				{
+					MoveCanvas(true);
+				}		
+			}
+		}
+
+		private void MoveCanvas(bool moveCanvasUp)
+		{
+			if (moveCanvas)
+			{
+				currentMovementTime += Time.deltaTime;
+
+				if (currentMovementTime > totalMovementTime)
+				{
+					currentMovementTime = totalMovementTime;
+					moveCanvas = false;
+				}
+
+				float t = currentMovementTime / totalMovementTime;
+				if (moveCanvasUp) //Move up should move at constant time, move down should move at smoothed time
+				{
+					canvasTransform.position = Vector3.Lerp(downPos, upPos, t);
+				} else
+				{
+					t = t * t * t * (t * (6f * t - 15f) + 10f);
+					canvasTransform.position = Vector3.Lerp(upPos, downPos, t);
+				}
+			} else
+			{
+				currentMovementTime = 0;
+				isCanvasUp = !isCanvasUp;
+				canMove = false;
+			}
 		}
 	}
 }
